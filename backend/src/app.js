bf3e1e5fdf859bc2d0b3618e930ff41404faaee0
Saurabh Kumar <akgec.saurabh@gmail.com>
2025-04-25 18:04:47 +0530

    - user routes setup

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 import express, { urlencoded } from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import userRoutes from "./routes/user.routes.js";
 const app = express();
 
 app.use(
@@ -15,6 +16,25 @@ app.use(cookieParser());
 // routes
 app.use("/api/v1/hello", (req, res) => {
   res.send("Hello World");
+});
+
+app.use("/api/v1/users", userRoutes);
+
+// not found middleware
+app.use((req, res, next) => {
+  const error = new Error(`Route ${req.originalUrl} not found!`);
+  error.status = 404;
+  next(error);
+});
+
+// error handling middleware
+app.use((error, req, res, next) => {
+  const statusCode = error.statusCode || 500;
+  const message = error.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    message,
+  });
 });
 
 export { app };
---
+++ backend/src/controllers/user.controller.js
@@ -0,0 +1,9 @@
+const signupUser = async (req, res) => {
+  res.send("Signup User");
+};
+
+const loginUser = async (req, res) => {
+  res.send("Login User");
+};
+
+export { signupUser, loginUser };
